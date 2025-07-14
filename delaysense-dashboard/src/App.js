ac22929ea0056faafb2ae0f51af4b002a11bce48
iteshprajapati <workwithitesh@gmail.com>
2025-07-14 17:45:14 +0530

    Updated sidebar.js

--- delaysense-dashboard/src/App.js
+++ delaysense-dashboard/src/App.js
@@ -2,16 +2,24 @@ import React from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar';
 import Dashboard from './components/Dashboard';
+import Settings from './components/Settings';  
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app-root">
-      <Sidebar />
-      <div className="main-area">
-        <Dashboard />
+    <Router>
+      <div className="app-root">
+        <Sidebar />
+        <div className="main-area">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
 export default App;
+
